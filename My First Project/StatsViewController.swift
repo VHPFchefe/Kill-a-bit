bd8f75b287694e3e3459577c86246afe5390a0b7
Vinícius Floriano <viniciushpfloriano@outlook.com>
2023-09-26 21:55:11 -0300

    Adicionando as Labels e ViewController de Status
    Ainda resta adicionar a lógica de acumula de status para alimentar a view

---
+++ My First Project/StatsViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class StatsViewController : ViewController {
+    
+    override func viewDidLoad() {
+        title = "Status"
+    }
+    
+    @IBOutlet weak var viewPoder : UILabel!
+    @IBOutlet weak var viewTotalOuroObtido : UILabel!
+    @IBOutlet weak var viewTotalMonstrosAbatidos : UILabel!
+    @IBOutlet weak var viewTotalChefesAbatidos : UILabel!
+    
+}
